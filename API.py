b6a5f8dd3000827501cca434e9c3321b014e321d
helloitsdaksh <helloitsdaksh007@gmail.com>
2021-05-23 12:59:53 +0530

    home get method initialised

--- API.py
+++ API.py
@@ -3,9 +3,18 @@ from flask_restful import Api, Resource,abort
 from flask import (Blueprint, current_app, flash, jsonify, redirect, request,url_for)
 import pandas as pd
 from flask_cors import CORS
-import os
 
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+class home(Resource):
+    def get(self):
+        x = ['+','-','/','*']
+        return {'API':'Calculator','Input': x},201
+
+
+api.add_resource(home,'/')
+if __name__ == "__main__":
+    app.run(debug = True)

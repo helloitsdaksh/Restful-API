e43d1b7ca0f2ace3afced6d9ab18458b5cdf9257
helloitsdaksh <helloitsdaksh007@gmail.com>
2021-05-23 13:22:30 +0530

    updated

--- Calculator_API.py
+++ Calculator_API.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource,abort
 from flask import (Blueprint, current_app, flash, jsonify, redirect, request,url_for)
-import pandas as pd
 from flask_cors import CORS
 
 
